2a546b4507291512b00fba734a5dfff3a6844abe
imjamam hossain <imjamamhossain68@gmail.com>
2024-11-25 03:11:28 +0600

    conditional statement commit

---
+++ if_else.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+func main() {
+	// num := -10
+	// if num > 0 {
+	// 	fmt.Println(num, "is positive")
+	// } else {
+	// 	fmt.Println(num, "is not positive")
+
+	// }
+
+	// 	num := 0
+	// 	if num > 0 {
+	// 		fmt.Println(num, "number is positive")
+
+	// 	} else if num == 0 {
+	// 		fmt.Println(num, "number is Zero")
+	// 	} else {
+	// 		fmt.Println(num, "number is negative")
+	// 	}
+
+	// nested if else
+	// num := 50
+	// if num > 0 {
+	// 	fmt.Println(num, "number is positive")
+
+	// } else {
+	// 	if num == 0 {
+	// 		fmt.Println(num, "number is Zero")
+	// 	} else {
+	// 		fmt.Println(num, "number is negative")
+	// 	}
+	// }
+
+	//if with function
+	num := -12
+	if isPositive(num) {
+		fmt.Println(num, "number is positive")
+	} else {
+		fmt.Println(num, "number is Neagtive")
+	}
+
+}
+
+func isPositive(num int) bool {
+	return num > 0
+}
