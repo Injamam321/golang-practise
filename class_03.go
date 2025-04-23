c8745fd839543533c4351238be33311a6757bfc6
Md. Injamam Hossain <injamam075@gmail.com>
2025-04-24 01:49:13 +0600

    class -03

--- class_03.go
+++ class_03.go
@@ -1,17 +1,45 @@
+// // package main
+
+// // import "fmt"
+
+// // func main() {
+// // 	num := 5
+
+// // 	if num > 0 {
+// // 		fmt.Println("Positive Number")
+// // 	} else if num < 0 {
+// // 		fmt.Println("Negative Number")
+// // 	} else {
+// // 		fmt.Println("Zero")
+// // 	}
+// // }
+
 // package main
 
 // import "fmt"
 
 // func main() {
-// 	num := 5
+// 	// age := 17
 
-// 	if num > 0 {
-// 		fmt.Println("Positive Number")
-// 	} else if num < 0 {
-// 		fmt.Println("Negative Number")
-// 	} else {
-// 		fmt.Println("Zero")
+// 	// if age >= 18 {
+// 	// 	fmt.Println("You can vote!")
+// 	// } else {
+// 	// 	fmt.Println("You are too young to vote.")
+// 	// }
+
+// 	day := "Monday"
+
+// 	switch day {
+// 	case "Monday":
+// 		fmt.Println("Start of the week")
+// 	case "Friday":
+// 		fmt.Println("Weekend is near")
+// 	case "Saturday", "Sunday":
+// 		fmt.Println("It's the weekend!")
+// 	default:
+// 		fmt.Println("Midweek day")
 // 	}
+
 // }
 
 package main
@@ -19,25 +47,19 @@ package main
 import "fmt"
 
 func main() {
-	// age := 17
+	marks := 85
 
-	// if age >= 18 {
-	// 	fmt.Println("You can vote!")
-	// } else {
-	// 	fmt.Println("You are too young to vote.")
-	// }
-
-	day := "Monday"
-
-	switch day {
-	case "Monday":
-		fmt.Println("Start of the week")
-	case "Friday":
-		fmt.Println("Weekend is near")
-	case "Saturday", "Sunday":
-		fmt.Println("It's the weekend!")
+	switch {
+	case marks >= 90:
+		fmt.Println("Grade: A+")
+	case marks >= 80:
+		fmt.Println("Grade: A")
+	case marks >= 70:
+		fmt.Println("Grade: B")
+	case marks >= 60:
+		fmt.Println("Grade: C")
 	default:
-		fmt.Println("Midweek day")
+		fmt.Println("Grade: F")
 	}
 
 }
