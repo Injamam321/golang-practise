6136bc2e89e0f3a45f1159ad5f8eeb7829fcc2e0
Md. Injamam Hossain <injamam075@gmail.com>
2025-04-24 01:36:51 +0600

    class -02

---
+++ class_02.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 10
+	b := 180
+
+	a = 305
+
+	b = 90
+
+	fmt.Println(a)
+	fmt.Println(b)
+}
+
+/*
+Data types of Go :
+1. int
+2. float32
+3. bool
+4. string
+
+*/
